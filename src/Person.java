dbd3889c4f99f1c7194b9fa7d8b566600cc65438
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-05 16:44:48 -0500

    Java Update

--- src/Person.java
+++ src/Person.java
@@ -1,23 +1,54 @@
 public class Person {
 
-    public String firstName;
-    public String lastName;
+    private String firstName;
+    private String lastName;
+
+    // class is blue print , idea of that person
+    // the object is the instance of that class
+    //Plain Old Java Object , cmnd n , getters and setters for all properties
+
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String getFullName() {
         return firstName + " " + lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+// command + n to create constructor, getters and setters
     public static void main(String[] args) {
-        Person rick = new Person();
+        Person rick = new Person("Armani", "Hall");
         //create a variable called Rick
-        rick.firstName = "Rick";
-        rick.lastName = "Sanchez";
-        Person shy = new Person();
-        shy.firstName = "Shy";
-        shy.lastName = "Hall";
+        Person shy = new Person("Shy", "Hall");
 
         System.out.println(rick.getFullName());
         System.out.println(shy.getFullName());
+
+
+
+
+
+        Person deshawn = new Person("DeShawn", "Hall");
+        System.out.println(deshawn.getFullName());
+
+
     }//end main
 
 
